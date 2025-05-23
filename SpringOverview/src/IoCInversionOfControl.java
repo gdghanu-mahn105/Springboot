e5f7f2dd2182a4741f42fe98b583c25f2c109d80
gdghanu-mahn105 <gdschanu.manhdd1005@gmail.com>
2025-05-23 22:13:33 +0700

    [Spring] what is IoC??

---
+++ SpringOverview/src/IoCInversionOfControl.java
@@ -0,0 +1,12 @@
+public class IoCInversionOfControl {
+    /*
+    Inversion of control: là nguyên lí trong đó quyền kiểm soát việc tạo và quản lí đối tượng
+    không nằm ở chính lớp sử dụng, mà được chuyển sang một phần khác
+    - khi không dùng IoC: class tự tạo và quản lí các dependencies
+    (chặt chẽ, khó test, khó bảo trì)
+    - dùng IoC: class nhận dependencies từ bên ngoài, không cần tự tạo
+    ( dễ thay đổi, dễ test, linh hoạt, dễ bảo trì, mở rộng )
+    IoC: là khái niệm, DI (Dependency injection) là kĩ thuật cụ thể để áp dụng nó.
+
+     */
+}
